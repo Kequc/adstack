0ec061973a74ad0c3bfe45ad44d2bae37028ea24
Nathan Lunde-Berry <nathan.lundeberry@gmail.com>
2012-07-12 20:25:01 +0200

    Support account lookup

--- lib/adstack/service/account_service.rb
+++ lib/adstack/service/account_service.rb
@@ -5,6 +5,17 @@ module Adstack
 
     customer_id_free
 
+    def self.find(amount=:all, params={})
+      result = super(:all, params)
+      customer_ids = Array.wrap(params[:customer_ids])
+      unless customer_ids.empty?
+        result = result.select {|a| customer_ids.include?(a.customer_id)}
+      end
+      result = result.first if amount == :first
+      result = result.sample if amount == :sample
+      result
+    end
+
     def selector
     end
 
