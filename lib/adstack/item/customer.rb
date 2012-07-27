48352f30819968c1aa082ec099d1f5e6c9ea2685
Nathan Lunde-Berry <nathan.lundeberry@gmail.com>
2012-07-27 22:30:23 +0200

    Remove extraneous field from customer

--- lib/adstack/item/customer.rb
+++ lib/adstack/item/customer.rb
@@ -1,14 +1,13 @@
 module Adstack
   class Customer < Item
 
-    field :customer_id,         :ro
     field :name,                :f, :roc, :s
     field :login,               :f, :ro, :s
     field :company_name,        :f, :ro, :s
+    field :customer_id,         :ro
     field :can_manage_clients,  :f, :ro, :s
     field :currency_code,       :f, :roc, :s, l: [3, 3]
     field :date_time_zone,      :f, :roc, :s
-    field :descriptive_name,    :ro
 
     service_api :managed_customer, r: :customer, p: :customer_id
 
