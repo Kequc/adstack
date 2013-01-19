9ef1f979de8cee1ab720d3db69e8c6c0bf20acb4
Nathan Lunde-Berry <mr.kequc@gmail.com>
2013-01-19 23:07:19 +0100

    GeoPoint class supports regular coordinates

--- lib/adstack/helper/geo_point.rb
+++ lib/adstack/helper/geo_point.rb
@@ -6,13 +6,16 @@ module Adstack
 
     def initialize(params={})
       params.symbolize_all_keys!
-      params[:latitude] ||= params[:latitude_in_micro_degrees]
-      params[:longitude] ||= params[:longitude_in_micro_degrees]
+      params[:latitude] ||= Adstack::Toolkit.largify(params[:latitude_in_micro_degrees])
+      params[:longitude] ||= Adstack::Toolkit.largify(params[:longitude_in_micro_degrees])
       super(ATTRIBUTES, params)
     end
 
     def writeable_attributes
-      { latitude_in_micro_degrees: self.latitude, longitude_in_micro_degrees: self.longitude }
+      {
+        latitude_in_micro_degrees: Adstack::Toolkit.microfy(latitude),
+        longitude_in_micro_degrees: Adstack::Toolkit.microfy(longitude)
+      }
     end
 
   end
